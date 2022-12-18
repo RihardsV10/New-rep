e4b9f4512d74e669a52bad7932ca3e2d2c67263b
RihardsV10 <rihardsvaldens@gmail.com>
2022-12-18 23:47:33 +0200

    123

---
+++ v.js
@@ -0,0 +1,7 @@
+document.querySelector("#some_unique_id").addEventListener("click", () => {
+    document.querySelector("#change_this_text").innerHTML = "Pieslēgties";
+  }, false);
+
+  document.querySelector("#some_unique_id").addEventListener("click", () => {
+    document.querySelector("#change_this_text2").innerHTML = "Ķimijas laboratorijas uzskaites sistēma";
+  }, false);
